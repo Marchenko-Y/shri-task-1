10c0a737839c0c36d3948d90efef50d0885a7c85
marchenko ulya <marchenko.ulya26@gmail.com>
2020-01-17 20:58:27 +0300

    first commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,28 @@
-"use strict";
-const onoffswitch_click_handler = function(e) {
-  e.target.classList.toggle("onoffswitch_checked");
-  const defaultThemeItems = document.querySelectorAll(
-    ".theme_color_project-default"
-  );
-  const inverseThemeItems = document.querySelectorAll(
-    ".theme_color_project-inverse"
-  );
-  for (let item of defaultThemeItems) {
-    item.classList.add("theme_color_project-inverse");
-    item.classList.remove("theme_color_project-default");
-  }
+document.addEventListener("click", function(e) {
+  if (event.target.classList.contains("onoffswitch")) {
+    e.target.classList.toggle("onoffswitch_checked");
+    const defaultThemeItems = document.querySelectorAll(
+      ".theme_color_project-default"
+    );
+    const inverseThemeItems = document.querySelectorAll(
+      ".theme_color_project-inverse"
+    );
+    for (let item of defaultThemeItems) {
+      item.classList.add("theme_color_project-inverse");
+      item.classList.remove("theme_color_project-default");
+    }
 
-  for (let item of inverseThemeItems) {
-    item.classList.add("theme_color_project-default");
-    item.classList.remove("theme_color_project-inverse");
+    for (let item of inverseThemeItems) {
+      item.classList.add("theme_color_project-default");
+      item.classList.remove("theme_color_project-inverse");
+    }
   }
-};
-const onoffswitchDOMElement = document.querySelector(".onoffswitch");
-onoffswitchDOMElement.addEventListener("click", onoffswitch_click_handler);
+});
+
+document.addEventListener("click", function(e) {
+  if (e.target.closest(".e-accordion__short")) {
+    e.target
+      .closest(".e-accordion__short")
+      .nextElementSibling.classList.toggle("e-accordion__more");
+  }
+});
